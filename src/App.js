cc4a8d0a9be8ce1e180c32afe0ffe536e5307674
Aaron Dennis <amdennis1987@gmail.com>
2020-09-25 12:40:17 -0600

    chore: add given assets and add .env example for api key

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react"
+import axios from "axios"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    const API_KEY = process.env.REACT_APP_API_KEY
+    const url = `https://api.data.gov/ed/collegescorecard/v1/schools.json?per_page=100&_fields=school.name,school.school_url,id,school.city,school.state,latest.student.size,latest.admissions.admission_rate.overall,latest.programs.cip_4_digit.credential,latest.programs.cip_4_digit.title&api_key=${API_KEY}`
+
+    useEffect(() => {
+        const fetchData = async function () {
+            try {
+                setLoading(true)
+                const response = await axios.get(url)
+                if (response.status === 200) {
+                    setData(response.data)
+                }
+            } catch (error) {
+                throw error
+            } finally {
+                setLoading(false)
+            }
+        }
+        fetchData()
+    }, [url])
+
+    console.log(data)
+
+    return (
+      <>
+        {loading && <div className="App">Hello</div>}
+      </>
+    );
 }
 
-export default App;
+export default App
