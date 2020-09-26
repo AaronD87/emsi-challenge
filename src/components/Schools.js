0ac57340a1eb92e026c123915cf915a2056763cf
Aaron Dennis <amdennis1987@gmail.com>
2020-09-26 14:39:31 -0600

    feat: add base markup with school data

--- src/components/Schools.js
+++ src/components/Schools.js
@@ -6,10 +6,12 @@ const Schools = ({ schools }) => {
     'school.name': schoolName,
     'school.state': schoolState,
     'school.city': schoolCity,
-    'school.admissions.admission_rate.overall': schoolAdmissions,
+    'latest.admissions.admission_rate.overall': schoolAdmissions,
     'latest.student.size': schoolStudentSize,
     'latest.programs.cip_4_digit': programCount,
   } = schools
+
+  console.log(12)
 
   const {
     undergraduateDegreeCount,
@@ -23,25 +25,38 @@ const Schools = ({ schools }) => {
   } = useProgramCounts(programCount)
 
   return (
-    <div>
-      {schoolName}
-      {/* {schoolState}
-            {schoolCity}
-            {schoolAdmissions} */}
-      {/* {schoolStudentSize} */}
-      {associateDegreeCount > 0 ? (
-        <p> Associate Degrees: {associateDegreeCount} </p>
-      ) : (
-        ''
-      )}
-      {bachelorDegreeCount > 0 ? (
-        <p> Bachelor Degrees: {bachelorDegreeCount} </p>
-      ) : (
-        ''
-      )}
-      {/* {undergraduateDegreeCount} */}
-      {/* {bachelorDegrees} */}
-    </div>
+    <ul className="wrapper">
+      <li className="card">
+        <section className="upper-card-wrapping">
+          <h2 className="school-title">{schoolName}</h2>
+          <p className="city-state"> {schoolCity}, {schoolState}</p>
+          <img src="https://foroakcliff.org/wp-content/uploads/2016/05/placeholder-160x160.png" className="logo" alt="logo"/>
+          <p className="student-size-header"> Student Size: </p>
+          <p className="student-size"> {schoolStudentSize} </p>
+          <p className="admission-rate-header"> Admission Rate: </p>
+          <div className="admission-rate-progress-bar-wrapper">
+            <p className="admission-rate"> {Math.round(schoolAdmissions * 100)}% </p>
+            <progress value={Math.round(schoolAdmissions * 100)} max="100" className="admission-rate-progress-bar"></progress>
+          </div>
+        </section>
+    
+    <hr />
+    
+        <section className="lower-card-wrapping">
+          <p className="program-count-header">
+            Available Program Counts: 
+          </p>
+          {undergraduateDegreeCount && <p className="degree-counts"> Undergraduate Degree: <strong>{undergraduateDegreeCount}</strong></p>}
+          <p className="degree-counts"> Associate Degree: <strong>{associateDegreeCount}</strong></p>
+          <p className="degree-counts"> Bachelors Degree: <strong>{bachelorDegreeCount}</strong></p>
+          <p className="degree-counts"> Post Baccalaureate Certificate: <strong>{postBaccalaureateCertificateCount}</strong></p>
+          <p className="degree-counts"> Masters Degree: <strong>{mastersDegreeCount}</strong></p>
+          <p className="degree-counts"> Doctoral Degree: <strong>{doctoralDegreeCount}</strong></p>
+          <p className="degree-counts"> First Professional Degree: <strong>{firstProfessionalDegreeCount}</strong></p>
+          <p className="degree-counts"> Graduate/Professional Certificate: <strong>{graduateProfessionalCertificateCount}</strong></p>
+        </section>
+      </li>
+    </ul>
   )
 }
 
