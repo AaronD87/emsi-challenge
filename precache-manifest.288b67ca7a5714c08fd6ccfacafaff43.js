f1e33617fddd5184a523c076d01940c11e97946f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-29 00:54:25 +0000

    Deploy AaronD87/emsi-challenge to github.com/AaronD87/emsi-challenge.git:gh-pages

---
+++ precache-manifest.288b67ca7a5714c08fd6ccfacafaff43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5895a9ce9c4003e15059c41fd0ec49b",
+    "url": "/emsi-challenge/index.html"
+  },
+  {
+    "revision": "17653546255d8dfd3547",
+    "url": "/emsi-challenge/static/css/main.196c7854.chunk.css"
+  },
+  {
+    "revision": "bd3afc6e177b070a8cb1",
+    "url": "/emsi-challenge/static/js/2.62d10953.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/emsi-challenge/static/js/2.62d10953.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17653546255d8dfd3547",
+    "url": "/emsi-challenge/static/js/main.fc828d39.chunk.js"
+  },
+  {
+    "revision": "1b3cc96edf3b2d4fee9c",
+    "url": "/emsi-challenge/static/js/runtime-main.57aa10eb.js"
+  },
+  {
+    "revision": "22792d7865fc1d123111c9252d98b7d9",
+    "url": "/emsi-challenge/static/media/university.22792d78.svg"
+  }
+]);
